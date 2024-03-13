844a1328cb73ddbbece41a451c470d8969c9cfa8
JIMMY VICENTE <jimmyvicentel@gmail.com>
2024-03-12 23:25:55 -0500

    first commit

---
+++ lib/pages/velocity/page_velocity.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:wifi_web/docs.dart';
+
+class PageVelocity extends StatefulWidget {
+  const PageVelocity({super.key});
+
+  @override
+  State<PageVelocity> createState() => _PageVelocityState();
+}
+
+class _PageVelocityState extends State<PageVelocity> {
+  ProviderConnection pvC = ProviderConnection.of();
+
+  @override
+  Widget build(BuildContext context) {
+    pvC = ProviderConnection.of(context, true);
+    return Scaffold(
+      body: Center(
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            children: [
+              AspectRatio(
+                aspectRatio: 2.5,
+                child:
+                    itemChartVelocity(pvC.listPoints, pvC.level, pvC.limitCount),
+              ),
+              const SizedBox(height: 10.0),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
